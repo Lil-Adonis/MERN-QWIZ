7816dc82e582f671cc7273826de5eed8ccc0aade
Lil-Adonis <adenugaib24@gmail.com>
2023-05-04 09:11:59 -1100

    :art: Quiz comp
    quiz

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Quiz = () => {
+
+
+
+export default function Quiz() {
+    /** next button event handler */
+    function onNext(){
+    console.log('hiiii')
+        }
+     
+
+    /** Prev button event handler */
+    function onPrev(){
+        console.log('hello')
+    }
+   
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+        <h1 className='title text-light'>Quiz Application</h1>
+
+        <div className='grid'>
+            <button className='btn prev' onClick={onPrev}>Prev</button>
+            <button className='btn next' onClick={onNext}>Next</button>
+        </div>
+    </div>
   )
 }
-
-export default Quiz
