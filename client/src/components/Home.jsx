faa7b15f7e4692d1ed08fccfbec0114c090cb312
Lil-Adonis <adenugaib24@gmail.com>
2023-05-04 08:40:47 -1100

    rules

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Welcome, Quiz Application</h1>
+        <ol>
+        <li>You will be asked 10 questions one after another.</li>
+            <li>10 points is awarded for the correct answer.</li>
+            <li>Each question has three options. You can choose only one options.</li>
+            <li>You can review and change answers before the quiz finish.</li>
+            <li>The result will be declared at the end of the quiz.</li>
+        </ol>
+    </div>
   )
 }
 
