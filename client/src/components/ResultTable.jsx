8f1e917b02a1fa9fd8909158ba98719a19526f8c
Lil-Adonis <adenugaib24@gmail.com>
2023-05-05 12:09:38 -1100

    result tabimport React, { useEffect, useState } from 'react'

--- client/src/components/ResultTable.jsx
+++ client/src/components/ResultTable.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ResultTable = () => {
+
+export default function ResultTable() {
+
+
+
   return (
-    <div>ResultTable</div>
+    <div>
+        <table>
+            <thead className='table-header'>
+                <tr className='table-row'>
+                    <td>Name</td>
+                    <td>Attemps</td>
+                    <td>Earn Points</td>
+                    <td>Result</td>
+                </tr>
+            </thead>
+            <tbody>
+                        <tr className='table-body' >
+                            <td>username</td>
+                            <td>attempts</td>
+                            <td>points</td>
+                            <td>achived</td>
+                        </tr>
+            </tbody>
+        </table>
+    </div>
   )
 }
-
-export default ResultTable
