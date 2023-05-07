904ab680ae5354858506e380bfb7ba335619ac63
Lil-Adonis <adenugaib24@gmail.com>
2023-05-06 22:03:16 -1100

    async

--- client/src/hooks/FetchQuestion.jsx
+++ client/src/hooks/FetchQuestion.jsx
@@ -10,6 +10,17 @@ export const useFetchQestion = () => {
     useEffect(() => {
         setGetData(prev => ({...prev, isLoading : true}));
 
+
+        /** async function fetch backend data */
+        (async () => {
+            try {
+                const [{ questions, answers }] = await getServerData(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/questions`, (data) => data)
+                
+                if(questions.length > 0){
+                    setGetData(prev => ({...prev, isLoading : false}));
+                    setGetData(prev => ({...prev, apiData : questions}));
+
+
         
 
 }
