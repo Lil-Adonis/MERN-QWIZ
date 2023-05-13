51f1813c51b520aa47f6f5d70c710e57de8a698b
Lil-Adonis <adenugaib24@gmail.com>
2023-05-13 10:25:19 -1100

    express setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log(hii)
+import express from 'express';
+
+const app = express()
+
+
+/** routes */
+app.get('/', (req,res) => {
+    try{
+        res.json('get request')
+    }catch(error){
+        res.json(error)
+    }
+})
