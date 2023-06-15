5dfde731b6b9f6e11e5473f9880742217e40af20
Lil-Adonis <adenugaib24@gmail.com>
2023-06-15 02:49:34 -1100

    backend

--- server/server.js
+++ server/server.js
@@ -1,31 +1,49 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
+import router from './router/route.js';
+
+
+/** import connection file */
+import connect from './database/conn.js';
 
 const app = express()
 
 
-/** App middleware */
+/** app middlewares */
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 config();
 
-/** App middleware */
-const port = process.env.PORT 
 
+/** appliation port */
+const port = process.env.PORT || 8080;
 
 
 /** routes */
-app.get('/', (req,res) => {
-    try{
-        res.json('get request')
-    }catch(error){
+app.use('/api', router) /** apis */
+
+
+app.get('/', (req, res) => {
+    try {
+        res.json("Get Request")
+    } catch (error) {
         res.json(error)
     }
 })
 
-app.listen (port, () => {
-    console.log(`server connected  http://localhost:${port}`)
+
+/** start server only when we have valid connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.log("Cannot connect to the server");
+    }
+}).catch(error => {
+    console.log("Invalid Database Connection");
 })
